463e92d38d5f8d14b250130346c628f586d40744
pavles6 <pavles6@protonmail.com>
2023-04-24 13:12:37 +0200

    Initialize projector from config and run methods that correspond to cli args

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
+import getConfig from "./config";
 import getOpts from "./opts";
+import Projector from "./projector";
 
-console.log(getOpts());
+async function main() {
+  const opts = getOpts();
+  const config = getConfig(opts);
+
+  const proj = await Projector.fromConfig(config);
+
+  if (config.operation === "Print") {
+    if (!config.args.length) {
+      console.log(JSON.stringify(proj.getValues()));
+    } else {
+      const value = proj.getValue(config.args[0]);
+      if (value) {
+        console.log(value);
+      } else {
+        console.log(
+          `No value found for key ${config.args[0]} at ${config.pwd}`
+        );
+      }
+    }
+  }
+
+  if (config.operation === "Add") {
+    proj.setValue(config.args[0], config.args[1]);
+    await proj.save();
+  }
+
+  if (config.operation === "Remove") {
+    proj.removeValue(config.args[0]);
+    await proj.save();
+  }
+}
+
+main();
